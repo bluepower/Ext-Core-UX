ae9eeed1bc79109c86f925c60c0bc33bff27a006
Niko Ni <bluepspower@163.com>
2010-02-22 01:01:00 +0800

    added ListPreview draft files

---
+++ ListPreview/scripts/ListPreview.js
@@ -0,0 +1,140 @@
+/*
+The MIT License
+
+Copyright (c) 2009 Niko Ni (bluepspower@163.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// namespace
+Ext.ns('Ext.ux');
+
+/**
+ * @class Ext.ux.ListPreview
+ * @extends-ext Ext.util.Observable
+ * @author Niko Ni (bluepspower@163.com)
+ * @demo http://cz9908.com/showcase/?item=listpreview&p=1
+ * @version v0.1
+ * @create 2010-02-21
+ * @update 2010-02-22
+ */
+Ext.ux.ListPreview = Ext.extend(Ext.util.Observable, {
+    //------------------------------------------------------------
+    // config options
+    //------------------------------------------------------------
+    /**
+     * @cfg {Array} items Array of listpreview config object items.
+     */
+    items : [],
+
+	/**
+	 * @cfg {Mixed} renderTo The container element.
+	 */
+	renderTo : document.body,
+
+    //------------------------------------------------------------
+    // class constructor
+    //------------------------------------------------------------
+    /**
+     * @constructor
+     * @param config
+	 * @private
+     */
+    constructor : function(config) {
+        Ext.apply(this, config);
+        Ext.ux.ListPreview.superclass.constructor.call(this);
+
+        // add custom event
+        this.addEvents(
+			/**
+		     * @event change
+			 * Fires when listpreview item is clicked
+			 * @param {Ext.ux.ListPreview} this
+			 * @param {Object} activeItem
+			 * @param {Number} index
+			 */
+			'change'
+		);
+
+        // initialize
+		this.init();
+    },
+
+    //------------------------------------------------------------
+    // public/private methods
+    //------------------------------------------------------------
+    /**
+     * @private
+     */
+    init : function() {
+		// properties
+        this.el = Ext.get(this.renderTo);
+
+		// init markup
+        this.initMarkup();
+
+		// init events
+        this.initEvents();
+    },
+
+    /**
+     * @private
+     */
+    initMarkup : function() {
+        this.containerEl = this.el.createChild({
+            tag : 'div',
+            cls : 'ux-list-preview-container'
+        });
+
+        var sId = this.el.id || Ext.id();
+
+        // build listpreview menu items
+        Ext.each(this.items, function(item, index) {
+            this.containerEl.createChild({
+                //tag : 'li',
+                cls : item.cls || '',
+                children : [{
+                    tag : 'a',
+                    id : sId + '-' + index,
+                    cls : 'ux-list-preview-item',
+                    href : item.url || '#',
+                    title : item.tip || item.title,
+                    html : item.title
+                }]
+            });
+        }, this);
+
+        this.menuItems = this.containerEl.select('a.ux-list-preview-item');
+    },
+    
+    /**
+     * @private
+     */
+    initEvents : function() {
+        this.menuItems.on('click', function(ev, t) {
+            if(t.href.slice(-1) == '#') {
+                ev.preventDefault();
+            }
+            //this.setCurrItem(Ext.get(t), true);
+            //var index = parseInt(t.id.split('-').pop(), 10);
+            //this.fireEvent('change', t, index);
+        }, this);
+    }
+
+});  // end of Ext.ux.ListPreview
