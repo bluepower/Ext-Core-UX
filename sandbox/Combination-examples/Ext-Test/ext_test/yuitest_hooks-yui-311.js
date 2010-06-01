d88bdc8a26f472322494321463d8e33947ba691c
Niko Ni <bluepspower@163.com>
2010-06-01 15:15:11 +0800

    remove old files

--- sandbox/Combination-examples/Ext-Test/ext_test/yuitest_hooks-yui-311.js
+++
@@ -1,8 +0,0 @@
-var Y = {};
-Y.ObjectAssert = {};
-
-YUI().use('test', function(A) {
-	// Some hooks
-	Y = A;
-	Y.ObjectAssert.hasProperty = Y.ObjectAssert.hasKey;
-});
--- sandbox/Combination-examples/Ext-Test/ext_test/yuitest_hooks-yui.js
+++
@@ -1,6 +0,0 @@
-// Current ExtJs Unit Test are based on YUI 3.1.1
-// Thanks to Niko for this
-YUI().use('*', function(Y) {
-	// Some hooks
-	window.Y = Y;
-});
