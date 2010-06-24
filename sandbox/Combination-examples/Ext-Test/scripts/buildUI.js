345f5b74ff97c397f3a75d587e40a7f6551352a0
bluepower <bluepspower@163.com>
2010-06-24 11:44:54 +0800

    add buttons to expand or collapse column tree nodes

--- sandbox/Combination-examples/Ext-Test/scripts/buildUI.js
+++ sandbox/Combination-examples/Ext-Test/scripts/buildUI.js
@@ -1,29 +1,50 @@
-      Ext.onReady(function(){
-        Ext.QuickTips.init();
-
-        var grid = new Ext.test.view.Logger({
-            region: 'south',
-            height: 200,
-            split: true,
-            minSize: 100
-        });
-
-        var viewer = new Ext.test.view.ColumnTree({
-            region: 'center',
-            title: document.title,
-            height: 350,
-            tbar: [{
-                xtype: 'teststartbutton'
-            }, '->', {
-                xtype: 'testprogressbar',
-                width: 500
-            }]
-        });
-
-        var testViewport = new Ext.Viewport({
-            layout : 'border',
-            items : [viewer,grid]
-        });
-
-        testViewport.show(); 
-      },this);
+Ext.onReady(function(){
+	Ext.QuickTips.init();
+	
+	var grid = new Ext.test.view.Logger({
+		region: 'south',
+		height: 200,
+		split: true,
+		minSize: 100
+	});
+		
+	var viewer = new Ext.test.view.ColumnTree({
+		region: 'center',
+		title: document.title,
+		height: 350,
+		tbar: [
+			{
+				xtype: 'teststartbutton',
+				tooltip: 'press to start to run tests'
+			},
+			'-',
+			{
+				iconCls: 'icon-expand-all',
+				tooltip: 'Expand All',
+				handler: function() {
+					viewer.getRootNode().expand(true);
+				}
+			},
+			'-',
+			{
+				iconCls: 'icon-collapse-all',
+				tooltip: 'Collapse All',
+				handler: function() {
+					viewer.getRootNode().collapse(true);
+				}
+			},
+			'->',
+			{
+				xtype: 'testprogressbar',
+				width: 500
+            }
+		]
+	});
+		
+	var testViewport = new Ext.Viewport({
+		layout : 'border',
+		items : [viewer, grid]
+	});
+		
+	testViewport.show(); 
+},this);
